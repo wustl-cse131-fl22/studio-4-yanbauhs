6ef986e684b2db21c1983242810eabff71d9dcad
sophiab <sophiab@localhost>
2022-10-12 15:16:15 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,23 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	StdDraw.clear();
+	
+	StdDraw.setPenColor(Color.YELLOW);
+	StdDraw.filledRectangle(0,0,100,100);
+	
+	StdDraw.setPenColor(Color.BLACK);
+	StdDraw.filledRectangle(.25, .75, .05, .05);
+	
+	StdDraw.setPenColor(Color.BLACK);
+	StdDraw.filledRectangle(.75, .75, .05, .05);
+	
+	StdDraw.setPenColor(Color.BLACK);
+	StdDraw.filledRectangle(.5, .5, .3, .05);
+	
+	
+	
+	
+	
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,34 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		System.out.println(shapeType);
+		
+		int redComponent = in.nextInt();
+		System.out.println(redComponent);
+		
+		int greenComponent = in.nextInt();
+		System.out.println(greenComponent);
+		
+		int blueComponent = in.nextInt();
+		System.out.println(blueComponent);
+		
+		boolean isFilled = in.nextBoolean();
+		System.out.println(isFilled);
+		
+		double x = in.nextDouble();
+		System.out.println(x);
+		
+		double y = in.nextDouble();
+		System.out.println(y);
+		
+		double halfWidth = in.nextDouble();
+		System.out.println(halfWidth);
+		
+		double halfHeigh = in.nextDouble();
+		System.out.println(halfHeigh);
+		
+		
+		
 	}
 }
